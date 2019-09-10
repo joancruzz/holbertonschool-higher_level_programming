541c3979cf33777eb1b1c628bae6e08434b25349
joancruzz <joancruz95@gmail.com>
2019-09-10 06:16:05 +0000

    Program that checks for cycle and header file

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * check_cycle - check if linked list has cycle
+ * @list: value
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *first = list;
+	listint_t *second = list;
+
+	if (list == NULL)
+		return (0);
+	while (first != NULL && second != NULL && second->next != NULL)
+	{
+		first = first->next;
+		second = second->next->next;
+		if (first == second)
+			return (1);
+	}
+	return (0);
+}
