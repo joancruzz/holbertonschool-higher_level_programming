834d1ee2f0991bfeb9fab7fe3f81b203d478dd49
joancruzz <joancruz95@gmail.com>
2019-09-11 07:10:31 +0000

    Print in all uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,9 @@
-i
+#!/usr/bin/python3
+def uppercase(str):
+    for uppr_lttr in str:
+        if ord(uppr_lttr) >= 97 and ord(uppr_lttr) <= 122:
+	        dif = 32
+        else:
+	        dif = 0
+        print("{:c}".format(ord(uppr_lttr) - dif), end="")
+    print( )
