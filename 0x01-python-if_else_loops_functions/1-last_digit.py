cfad143a521b7c8e553a40222280c51b549decda
joancruzz <joancruz95@gmail.com>
2019-09-11 04:03:07 +0000

    Changed to else statement

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -15,5 +15,5 @@ if last_digit > 5:
         print("Last digit of {:d} is {:d} and is greater than 5".format(num_copy,last_digit))
 elif last_digit == 0:
     print("Last digit of {:d} is 0 and is 0".format(num_copy, last_digit))
-elif last_digit < 6:
+else:
         print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(num_copy, last_digit))
