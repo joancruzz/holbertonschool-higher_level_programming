7b306bf41f4b596206ce83e359886395a9ddc8b6
joancruzz <joancruz95@gmail.com>
2019-09-11 05:28:02 +0000

    Add new line after strings

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,8 +8,8 @@ last_digit = number % 10
 if num_copy < 0:
     last_digit= -last_digit
 if last_digit > 5:
-    print("Last digit of {:d} is {:d} and is greater than 5".format(num_copy,last_digit))
+    print("Last digit of {:d} is {:d} and is greater than 5".format(num_copy,last_digit), end="")
 elif last_digit == 0:
-    print("Last digit of {:d} is 0 and is 0".format(num_copy, last_digit))
+    print("Last digit of {:d} is 0 and is 0".format(num_copy, last_digit), end="")
 else:
-    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(num_copy, last_digit))
+    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(num_copy, last_digit), end="")
