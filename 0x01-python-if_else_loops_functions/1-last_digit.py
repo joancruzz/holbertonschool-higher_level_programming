b98e51075c0ac6708be9d63a6b0a1edee47cc9f6
joancruzz <joancruz95@gmail.com>
2019-09-11 05:33:44 +0000

    Append strings

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,8 +8,8 @@ last_digit = number % 10
 if num_copy < 0:
     last_digit= -last_digit
 if last_digit > 5:
-    print("Last digit of {:d} is {:d} and is greater than 5".format(num_copy,last_digit), end="")
+    print("Last digit of {:d} is {:d} and is greater than 5" % (num_copy,last_digit))
 elif last_digit == 0:
-    print("Last digit of {:d} is 0 and is 0".format(num_copy, last_digit), end="")
+    print("Last digit of {:d} is 0 and is 0" % (num_copy, last_digit))
 else:
-    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(num_copy, last_digit), end="")
+    print("Last digit of {:d} is {:d} and is less than 6 and not 0" % (num_copy, last_digit))
