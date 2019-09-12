c57cf4ed705233c97d110b5ad7d06b5ac7b267ea
joancruzz <joancruz95@gmail.com>
2019-09-12 03:49:06 +0000

    Change variable names to be PEP8 compliant

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-num_copy = number
+num = number
 if number < 0:
     number = -number
-last_digit = number % 10
-if num_copy < 0:
-    last_digit= -last_digit
-if last_digit > 5:
-    print("Last digit of {} is {} and is greater than 5".format(num_copy,last_digit))
-elif last_digit == 0:
-    print("Last digit of {} is 0 and is 0".format(num_copy, last_digit))
+ld = number % 10
+if num < 0:
+    ld = -ld
+if ld > 5:
+    print("Last digit of {} is {} and is greater than 5".format(num, ld))
+elif ld == 0:
+    print("Last digit of {} is 0 and is 0".format(num, ld))
 else:
-    print("Last digit of {} is {} and is less than 6 and not 0".format(num_copy, last_digit))
+    print("Last digit of %d is %d and is less than 6 and not 0" % (num, ld))
