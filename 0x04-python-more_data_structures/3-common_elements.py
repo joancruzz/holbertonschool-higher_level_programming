d501de3e7238d15fbd716ddd5eb1ef1ef95ebad6
Joan <joancruz95@gmail.com>
2019-09-19 23:49:55 +0000

    Add append

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
+    set_3 = []
     for x in set_1:
         for y in set_2:
             if x == y:
-                return y
-    return x
+                set_3.append(x)
+    return (set_3)
+            
