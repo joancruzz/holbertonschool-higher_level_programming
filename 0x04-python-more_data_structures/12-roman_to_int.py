251421cbca0eab6cb1732cfffc85fe32372617bf
joancruzz <51678245+joancruzz@users.noreply.github.com>
2019-09-19 22:29:00 -0700

    Update 12-roman_to_int.py
    Edit variable name

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,10 +3,10 @@ def roman_to_int(roman_string):
     my_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     value = 0
     num = 0
-    numeral = rom_str
-    if type(rom_str) is not str:
+    numeral = roman_string
+    if type(rom_string) is not str:
         return 0
-    if rom_str is None:
+    if roman_string is None:
         return 0
     for num in range (num, len(numeral)):
         if num < len(numeral) - 1 and my_dict[numeral[num]] < my_dict[numeral[num + 1]]:
