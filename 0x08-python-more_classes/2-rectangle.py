354ffa77b64f7bee40646021386b84739a6f10c9
joancruzz <joancruz95@gmail.com>
2019-10-07 23:36:49 +0000

    Add edgecase

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -41,4 +41,6 @@ class Rectangle:
 
     def perimeter(self):
         """Return perimeter of rectangle"""
+        if self.__width or self.__height =- 0:
+            return 0
         return 2*(self.width + self.__height)
