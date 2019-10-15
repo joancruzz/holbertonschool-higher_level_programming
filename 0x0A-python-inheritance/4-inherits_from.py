a9f2a698c2950a706bb38f406fe2e3d9473f8ef3
joancruzz <joancruz95@gmail.com>
2019-10-15 23:16:57 +0000

    Checks if instance of a class is inherited from another class

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
 def inherits_from(obj, a_class):
+    """Return true or false based on inheritance"""
+    if isinstance(obj, a_class) is True and type(obj) != a_class:
+        return True
+
+    else:
+        if isinstance(obj, a_class) is True and type(obj) != a_class:
+            return True
+        else:
+            return False
