35a75baa224455e52e8865a183c5ff2e9234fbb1
joancruzz <joancruz95@gmail.com>
2019-10-16 00:45:13 +0000

    Create class sqaure inherited from rectangle

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Create new class from Rectangle"""
+
+    def __init__(self, size):
+        """Initialize instance"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
