f87e292ef09484847bc9c1e4d23fcc2aa265018e
joancruzz <joancruz95@gmail.com>
2019-10-16 22:03:46 +0000

    Read lines and count num of lines

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
+"""This module defines the read_lines function"""
+
+
 def read_lines(filename="", nb_lines=0):
+    """Returns the number of lines in a textfile"""
+    with open(filename, 'r') as f:
+        if nb_lines <= 0:
+            print(f.read(), end='')
+            return
+        count = 0
+        for line in f:
+            if count < nb_lines:
+                print(line, end='')
+            count += 1
