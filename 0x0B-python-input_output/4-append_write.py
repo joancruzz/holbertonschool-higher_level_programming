502d0e7631081fa196c6f543178d6727a7addd92
joancruzz <joancruz95@gmail.com>
2019-10-16 22:32:35 +0000

    Append string and count its chars

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
+"""This module defines the append_write function"""
+
+
 def append_write(filename="", text=""):
+    """Returns num of chars added to appneded str"""
+    with open(filename, 'a') as f:
+        return f.write(str(text))
