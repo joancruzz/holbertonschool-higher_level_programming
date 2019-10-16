9e746c9484b93e4dab1513a412256357ac407382
joancruzz <joancruz95@gmail.com>
2019-10-16 20:05:39 +0000

    Print text file to stout

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
+"""
+This module defines the read_file function
+"""
+
+
 def read_file(filename=""):
+    """Print file text to stout"""
+    with open(filename, 'r') as f:
+        print(f.read(), end='')
