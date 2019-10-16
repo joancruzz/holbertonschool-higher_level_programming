c8cd12dd4df7ebd4ec7eabb0eb79c3f70ccbff5d
joancruzz <joancruz95@gmail.com>
2019-10-16 22:26:10 +0000

    Return number of chars in a string

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
+"""This module defines the write_lines function"""
+
+
 def write_file(filename="", text=""):
+    """Return a new string and number of letters in it"""
+    with open(filename, 'w') as f:
+        return f.write(str(text))
