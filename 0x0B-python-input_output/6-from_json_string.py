a7fb2be3ca83e8c7723c1f178fd93395774a6ba8
joancruzz <joancruz95@gmail.com>
2019-10-16 22:53:25 +0000

    returns an obj

--- 0x0B-python-input_output/6-from_json_string.py
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
+"""This module defines the from_json_string function"""
+
+import json
+
+
 def from_json_string(my_str):
+    """Returns JSON representation of an object"""
+    return json.loads(my_str)
