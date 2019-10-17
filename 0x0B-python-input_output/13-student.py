2fe34b8ad21fb26f93c72ab86e8d248f238aa0c8
joancruzz <joancruz95@gmail.com>
2019-10-17 17:29:53 +0000

    REplace attribute of student instance

--- 0x0B-python-input_output/13-student.py
+++ 0x0B-python-input_output/13-student.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """Initialize class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieve a dict"""
+        st = {}
+        if attrs is None:
+            return self.__dict__
+        for key in attrs:
+            if key in self.__dict__.keys():
+                st[key] = self.__dict__[key]
+        return st
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the student instance"""
+        for key in json:
+            setattr(self, key, json[key])
