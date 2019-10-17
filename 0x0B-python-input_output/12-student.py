207ccde71574910cfba9034d343d6916f4f92bf5
joancruzz <joancruz95@gmail.com>
2019-10-17 03:44:36 +0000

    defines students

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """Initialize class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieve a dict"""
+        st = {}
+        if attrs is None:
+            return self.__dict__
+        for key in attrs:
+            if key in self.__dict__.keys():
+                st[key] = self.__dict__[key]
+        return st
