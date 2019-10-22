d82d2ead3afbf13f433e75ba67a4d5a4b3ce3711
joancruzz <joancruz95@gmail.com>
2019-10-22 21:27:11 +0000

    edit sline split

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -113,5 +113,5 @@ class Rectangle(Base):
     def to_dictionary(self):
         """Return dict representation of Rectangle"""
         d = {'id': self.id, 'width': self.width,
-                'height': self.height, 'x': self.x, 'y': self.y}
+             'height': self.height, 'x': self.x, 'y': self.y}
         return d
