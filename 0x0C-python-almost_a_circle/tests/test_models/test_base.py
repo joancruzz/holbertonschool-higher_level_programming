5e4f6b68e7fe887bdbeab69aa94a904db6c09657
joancruzz <joancruz95@gmail.com>
2019-10-23 02:58:02 +0000

    edit

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python3
-"""
-Unittesting for Class - Base
-models/base.py
-"""
-
-import sys
-import unittest
+"""Unittest for Base"""
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+import os
+import unittest
+import sys
+from io import StringIO
 
-class BaseTest(unittest.TestCase):
-    """Class Base unittests"""
 
-
-    def test_incrementation(self):
-        """sees if object id increments """     
+class TestBase(unittest.TestCase):
+    """Base class test"""
+    def setUp(self):
+        """Resets nb_objects"""
         Base._Base__nb_objects = 0
 
+    def test_type(self):
+        """type"""
+        b1 = Base()
+        self.assertTrue(type(b1) == Base)
+
+    def test_id(self):
+        """Prints id"""
         b1 = Base()
         self.assertEqual(b1.id, 1)
 
@@ -25,15 +31,291 @@ class BaseTest(unittest.TestCase):
         b3 = Base()
         self.assertEqual(b3.id, 3)
 
-    def test_direct_inputs(self):
-        """checks for various valid inputs """
-        Base._Base__nb_objects = 0
+        b4 = Base(12)
+        self.assertEqual(b4.id, 12)
 
-        b_zero = Base()
-        self.assertEqual(b_zero.id, 1)
+        b5 = Base()
+        self.assertEqual(b5.id, 4)
 
-        b_int = Base(12)
-        self.assertEqual(b_int.id, 12)
+    def test_id_string(self):
+        """string"""
+        b1 = Base("string")
+        self.assertEqual(b1.id, "string")
 
-        b_neg_int = Base(-12)
-        self.assertEqual(b_neg_int.id, -12)
+    def test_id_None(self):
+        """None"""
+        b1 = Base(None)
+        self.assertEqual(b1.id, 1)
+
+    def test_id_float(self):
+        """float"""
+        b1 = Base(1.5)
+        self.assertEqual(b1.id, 1.5)
+
+    def test_id_NaN(self):
+        """nan"""
+        b1 = Base(float("nan"))
+        self.assertEqual(b1.id is float("nan"), False)
+
+    def test_id_inf(self):
+        """inf"""
+        b1 = Base(float("inf"))
+        self.assertEqual(b1.id is float("inf"), False)
+
+    def test_unknown(self):
+        """name error"""
+        with self.assertRaises(NameError):
+            Base(a)
+
+    def test_to_json_string_len(self):
+        """to_json_string len"""
+        r1 = Rectangle(10, 7, 2, 8)
+        dictionary = r1.to_dictionary()
+        json_dictionary = Base.to_json_string([dictionary])
+        self.assertEqual(len(json_dictionary), len(str([{
+            "x": 2, "width": 10, "id": 6, "height": 7, "y": 8}])))
+        self.assertTrue(type(json_dictionary), dict)
+
+    def test_to_json_string_len_square(self):
+        """to_json_string len"""
+        r1 = Square(10, 7, 2, 8)
+        dictionary = r1.to_dictionary()
+        json_dictionary = Base.to_json_string([dictionary])
+        self.assertEqual(len(json_dictionary), len(str([{
+            "x": 7, "size": 10, "id": 8, "y": 2}])))
+        self.assertTrue(type(json_dictionary), dict)
+
+    def test_to_json_string_type(self):
+        """to_json_string type"""
+        r1 = Rectangle(10, 7, 2, 8)
+        dictionary = r1.to_dictionary()
+        json_dictionary = Base.to_json_string([dictionary])
+        self.assertTrue(type(json_dictionary) is str)
+
+    def test_to_json_string_None(self):
+        """to_json_string type"""
+        json_dictionary = Base.to_json_string(None)
+        self.assertEqual(json_dictionary, "[]")
+
+    def test_to_json_string_empty(self):
+        """to_json_string empty"""
+        son_dictionary = Base.to_json_string('')
+        self.assertEqual(json_dictionary, '[]')
+
+    def test_save_to_file_len(self):
+        """JSON string representation length"""
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        Rectangle.save_to_file([r1, r2])
+        with open("Rectangle.json") as file:
+            self.assertEqual(
+                len(file.read()), len(str(
+                    [{"x": 2, "id": 6, "width": 10, "y": 8, "height": 7},
+                     {"x": 0, "id": 7, "width": 2, "y": 0, "height": 4}])))
+
+    def test_save_to_file_rec(self):
+        """JSON string rectangular"""
+        Rectangle.save_to_file([])
+        with open("Rectangle.json") as file:
+            self.assertEqual(file.read(), "[]")
+
+    def test_save_to_file_square(self):
+        """JSON string square"""
+        Square.save_to_file([])
+        with open("Square.json") as file:
+            self.assertEqual(file.read(), "[]")
+            os.remove("Square.json")
+
+    def test_save_to_file_None(self):
+        """JSON string None"""
+        Square.save_to_file(None)
+        with open("Square.json") as file:
+            self.assertEqual(file.read(), "[]")
+            os.remove("Square.json")
+
+    def test_save_to_file_len_Square(self):
+        """JSON string rep len"""
+        r1 = Square(10, 7, 2, 8)
+        r2 = Square(2, 4)
+        Square.save_to_file([r1, r2])
+        with open("Square.json") as file:
+            self.assertEqual(
+                len(file.read()), len(str(
+                    [{"x": 7, "id": 8, "size": 10, "y": 2}, {
+                        "x": 4, "id": 7, "size": 2, "y": 0}]
+                    )))
+
+    def test_save_to_file_None(self):
+        """JSON string length"""
+        Rectangle.save_to_file(None)
+        with open("Rectangle.json") as file:
+            self.assertEqual(file.read(), "[]")
+
+    def test_from_json_string(self):
+        """JSON string"""
+        list_input = [
+            {'id': 89, 'width': 10, 'height': 4},
+            {'id': 7, 'width': 1, 'height': 7}
+        ]
+        json_list_input = Rectangle.to_json_string(list_input)
+        list_output = Rectangle.from_json_string(json_list_input)
+        self.assertEqual(list_output, [{
+            'id': 89, 'width': 10, 'height': 4}, {
+            'id': 7, 'width': 1, 'height': 7}])
+        self.assertTrue(type(list_output), list)
+
+    def test_from_json_string_rec(self):
+        """JSON string rep"""
+        list_input = [
+            {'id': 89, 'size': 10},
+            {'id': 7, 'size': 1}
+        ]
+        json_list_input = Square.to_json_string(list_input)
+        list_output = Square.from_json_string(json_list_input)
+        self.assertEqual(list_output, [{
+            'id': 89, 'size': 10}, {
+            'id': 7, 'size': 1}])
+        self.assertTrue(type(list_output), list)
+
+    def test_from_json_string_None(self):
+        """JSON string None"""
+        list_input = [
+            {'id': 89, 'width': 10, 'height': 4},
+            {'id': 7, 'width': 1, 'height': 7}
+        ]
+        json_list_input = Rectangle.to_json_string(list_input)
+        list_output = Rectangle.from_json_string(None)
+        self.assertEqual(list_output, [])
+
+    def test_from_json_string_empty(self):
+        """JSON string None"""
+        list_input = [
+            {'id': 89, 'width': 10, 'height': 4},
+            {'id': 7, 'width': 1, 'height': 7}
+        ]
+        json_list_input = Rectangle.to_json_string(list_input)
+        list_output = Rectangle.from_json_string('')
+        self.assertEqual(list_output, [])
+
+    def test_from_json_string_empty(self):
+        """JSON string type"""
+        list_input = [
+            {'id': 89, 'width': 10, 'height': 4},
+            {'id': 7, 'width': 1, 'height': 7}
+        ]
+        json_list_input = Rectangle.to_json_string(list_input)
+        list_output = Rectangle.from_json_string('')
+        self.assertTrue(type(list_output), list)
+
+    def test_create(self):
+        """instance with set attributes"""
+        output = StringIO()
+        sys.stdout = output
+        r1 = Rectangle(3, 5, 1)
+        r1_dictionary = r1.to_dictionary()
+        r2 = Rectangle.create(**r1_dictionary)
+        print(r1)
+        sys.stdout = sys.__stdout__
+        self.assertEqual(output.getvalue(), "[Rectangle] (1) 1/0 - 3/5\n")
+        self.assertTrue(type(r1) == Rectangle)
+
+        output = StringIO()
+        sys.stdout = output
+        r1 = Square(3)
+        r1_dictionary = r1.to_dictionary()
+        r2 = Square.create(**r1_dictionary)
+        print(r1)
+        sys.stdout = sys.__stdout__
+        self.assertEqual(output.getvalue(), "[Square] (3) 0/0 - 3\n")
+        self.assertTrue(type(r1) == Square)
+
+    def test_create2(self):
+        """instance with set attributes"""
+        output = StringIO()
+        sys.stdout = output
+        r1 = Rectangle(3, 5, 1)
+        r1_dictionary = r1.to_dictionary()
+        r2 = Rectangle.create(**r1_dictionary)
+        print(r2)
+        sys.stdout = sys.__stdout__
+        self.assertEqual(output.getvalue(), "[Rectangle] (1) 1/0 - 3/5\n")
+
+    def test_create3(self):
+        """instance with set attributes"""
+        r1 = Rectangle(3, 5, 1)
+        r1_dictionary = r1.to_dictionary()
+        r2 = Rectangle.create(**r1_dictionary)
+        self.assertFalse(r1 is r2)
+        self.assertFalse(r1 == r2)
+
+    def test_create_TypeError(self):
+        """instance with TypeError"""
+        r1 = Rectangle(3, 5, 1)
+        r1_dictionary = r1.to_dictionary()
+        with self.assertRaises(TypeError):
+            r2 = Rectangle.create(None)
+
+    def test_create_TypeError_int(self):
+        """instance with TypeError int"""
+        r1 = Rectangle(3, 5, 1)
+        r1_dictionary = r1.to_dictionary()
+        with self.assertRaises(TypeError):
+            r2 = Rectangle.create(1, 2, 3)
+
+    def test_create_TypeError_string(self):
+        """instance with TypeError string"""
+        r1 = Rectangle(3, 5, 1)
+        r1_dictionary = r1.to_dictionary()
+        with self.assertRaises(TypeError):
+            r2 = Rectangle.create("string")
+
+    def test_create_TypeError_string(self):
+        """instance with TypeError string"""
+        r1 = Rectangle(3, 5, 1)
+        r1_dictionary = r1.to_dictionary()
+        with self.assertRaises(NameError):
+            r2 = Rectangle.create(**betty)
+
+    def test_load_from_file(self):
+        """list of instances"""
+        output = StringIO()
+        sys.stdout = output
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        list_rectangles_input = [r1, r2]
+        Rectangle.save_to_file(list_rectangles_input)
+        list_rectangles_output = Rectangle.load_from_file()
+
+        print(list_rectangles_output[0])
+        sys.stdout = sys.__stdout__
+        self.assertEqual(output.getvalue(), "[Rectangle] (1) 2/8 - 10/7\n")
+
+        output = StringIO()
+        sys.stdout = output
+        print(list_rectangles_output[1])
+        sys.stdout = sys.__stdout__
+        self.assertEqual(output.getvalue(), "[Rectangle] (2) 0/0 - 2/4\n")
+        self.assertTrue(type(list_rectangles_output), list)
+
+        output = StringIO()
+        sys.stdout = output
+        s1 = Square(5)
+        s2 = Square(7, 9, 1)
+        list_squares_input = [s1, s2]
+        Square.save_to_file(list_squares_input)
+        list_squares_output = Square.load_from_file()
+
+        print(list_squares_output[0])
+        sys.stdout = sys.__stdout__
+        self.assertEqual(output.getvalue(), "[Square] (5) 0/0 - 5\n")
+
+        output = StringIO()
+        sys.stdout = output
+        print(list_squares_output[1])
+        sys.stdout = sys.__stdout__
+        self.assertEqual(output.getvalue(), "[Square] (6) 9/1 - 7\n")
+        self.assertTrue(type(list_squares_output), list)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -1,28 +1,192 @@
 #!/usr/bin/python3
-"""
-Unittest for Square Size
-"""
+"""Unittest for Square"""
 import unittest
+import sys
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
+from io import StringIO
 
 
-class TestSquareSize(unittest.TestCase):
-        """Test Square Size Method"""
-        def test_size(self):
-            self.assertEqual(size(0), 0)
-            self.assertEqual(size(1), 1)
-            self.assertEqual(size(1.5), 1.5)
-            self.assertEqual(size(1), 1)
+class TestSquare(unittest.TestCase):
+    """Square class test"""
 
-        def test_value(self):
-            self.assertRaises(ValueError, size, -1)
+    def setUp(self):
+        """Resets nb_objects"""
+        Base._Base__nb_objects = 0
 
-        def test_type(self):
-            self.assertRaiseseRegex(TypeError, size, True)
-            self.assertRaises(TypeError, size, False)
-            self.assertRaises(TypeError, size, str)
-            self.assertRaises(TypeError, size, [])
-            self.assertRaises(TypeError, size, {})
+    def test_id(self):
+        """Prints id"""
+        r1 = Square(4)
+        self.assertEqual(r1.id, 1)
 
+        r2 = Square(2)
+        self.assertEqual(r2.id, 2)
+
+        r3 = Square(9, 0, 0, 13)
+        self.assertEqual(r3.id, 13)
+        self.assertTrue(type(r3), Square)
+
+    def test_all_param(self):
+        """Passing all parameters"""
+        r1 = Square(1, 2, 3, 4)
+
+    def test_all_negative(self):
+        """Passing all negative"""
+        with self.assertRaises(ValueError):
+            r1 = Square(-1)
+        with self.assertRaises(ValueError):
+            r1 = Square(1, -1, 2, 3)
+        with self.assertRaises(ValueError):
+            r1 = Square(1, 2, -3)
+
+    def test_all_zero(self):
+        """zero"""
+        with self.assertRaises(ValueError):
+            r1 = Square(0)
+
+    def test_string(self):
+        """string"""
+        with self.assertRaises(TypeError):
+            r1 = Square("string")
+        with self.assertRaises(TypeError):
+            r1 = Square(1, "2")
+        with self.assertRaises(TypeError):
+            r1 = Square(1, 2, "3")
+
+    def test_no_param(self):
+        """empty"""
+        with self.assertRaises(TypeError):
+            r1 = Square()
+      
+    def test_excess_param(self):
+        """Extra parameters"""
+        with self.assertRaises(TypeError):
+            r1 = Square(1, 2, 3, 4, 1, 3)
+
+    def test_float(self):
+        """Float"""
+        with self.assertRaises(TypeError):
+            r1 = Square(1.2)
+                                                          
+    def test_NaN(self):
+        """NaN"""
+        with self.assertRaises(TypeError):
+            r1 = Square(float("nan"))
+
+    def test_inf(self):
+        """inf parameter"""
+        with self.assertRaises(TypeError):
+            r1 = Square(float("inf"))
+                      
+    def test_unknown(self):
+        """unknown"""
+        with self.assertRaises(NameError):
+            r1 = Square(a)
+
+    def test_None(self):
+        """None"""
+        with self.assertRaises(TypeError):
+            r1 = Square(None)
+
+    def test_area(self):
+        """Print area"""
+        r1 = Square(10)
+        self.assertTrue(type(r1), Square)
+
+    def test_display(self):
+        """rectangle output"""
+        output = StringIO()
+        sys.stdout = output
+        r1 = Square(2)
+        r1.display()
+        sys.stdout = sys.__stdout__
+        assert output.getvalue() == "##\n##\n"
+
+    def test_str(self):
+        """__str__"""
+        output = StringIO()
+        sys.stdout = output
+        r1 = Square(4, 1, 2, 13)
+        print(r1)
+        sys.stdout = sys.__stdout__
+        assert output.getvalue() == "[Square] (13) 1/2 - 4\n"
+
+    def test_display_x_y(self):
+        """rectangle with x and y"""
+        output = StringIO()
+        sys.stdout = output
+        r2 = Square(2, 1, 0)
+        r2.display()
+        sys.stdout = sys.__stdout__
+        assert output.getvalue() == " ##\n ##\n"
+
+    def test_update(self):
+        """update"""
+        output = StringIO()
+        sys.stdout = output
+        r1 = Square(10, 10, 10)
+        r1.update(89)
+        r1.update(89, 2)
+        r1.update(89, 2, 3)
+        r1.update(89, 2, 3, 4)
+        print(r1)
+        sys.stdout = sys.__stdout__
+        assert output.getvalue() == "[Square] (89) 3/4 - 2\n"
+
+    def test_update_extra(self):
+        """Update with extra parameters"""
+        output = StringIO()
+        sys.stdout = output
+        r1 = Square(10, 10, 10)
+        r1.update(89, 3, 4, 5, 6)
+        print(r1)
+        sys.stdout = sys.__stdout__
+        assert output.getvalue() == "[Square] (89) 4/5 - 3\n"
+
+    def test_update_no_param(self):
+        """Update with no parameters"""
+        output = StringIO()
+        sys.stdout = output
+        r1 = Square(10, 10, 10)
+        r1.update()
+        print(r1)
+        sys.stdout = sys.__stdout__
+        assert output.getvalue() == "[Square] (1) 10/10 - 10\n"
+
+    def test_kwargs(self):
+        """kwargs"""
+        output = StringIO()
+        sys.stdout = output
+        r1 = Square(2, 2, 2)
+        r1.update(x=1, size=2, y=3)
+        print(r1)
+        sys.stdout = sys.__stdout__
+        assert output.getvalue() == "[Square] (1) 1/3 - 2\n"
+
+    def test_kwargs_extra_keys(self):
+        """kwargs normal behavior"""
+        output = StringIO()
+        sys.stdout = output
+        r1 = Square(2, 2, 2, 2)
+        r1.update(id=1, x=1, size=2, y=3, banu=89)
+        print(r1)
+        sys.stdout = sys.__stdout__
+        assert output.getvalue() == "[Square] (1) 1/3 - 2\n"
+
+    def test_to_dictionary(self):
+        """to dictionary"""
+        s1 = Square(10, 2, 1)
+        s1_dictionary = s1.to_dictionary()
+        self.assertEqual(s1_dictionary, {'id': 1, 'x': 2, 'size': 10, 'y': 1})
+        self.assertTrue(type(s1_dictionary), dict)
+
+    def test_save_to_file_None2(self):
+        """JSON string None"""
+        Square.save_to_file(None)
+        with open("Square.json") as file:
+            self.assertEqual(file.read(), "[]")
+
+
+if __name__ == '__main__':
+    unittest.main()
