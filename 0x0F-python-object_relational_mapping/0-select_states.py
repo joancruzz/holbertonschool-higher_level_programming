028e0ebed5c0ce71c743735138f4a8e0e49b3459
joan <joancruz95@gmail.com>
2019-12-16 10:05:38 +0000

    Edit pep8 issues

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,10 @@
 if __name__ == "__main__":
 
-    import MySQLdb
+    import MySQLdb
+    from sys import argv
 
-    from sys import argv
-
-
-
-
-    db = MySQLdb.connect(host="localhost",
-
-                         port=3306,
-
-                         user=argv[1],
-
-                         passwd=argv[2],
-
-                         db=argv[3])
+    db = MySQLdb.connect(host="localhost",
+                         port=330, user=argv[1], passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
 
@@ -24,7 +13,5 @@ if __name__ == "__main__":
     db.close()
 
     rows = cur.fetchall()
-
-    for row in rows:
-
-        print("{}".format(row))
+for row in rows:
+    print("{}".format(row))
