74945914a8e3451bbd916fb1da31649f3f2599ff
joan cruz <joancruz95@gmail.com>
2019-12-17 06:08:44 +0000

    Change databases to table

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,2 @@
--- Script that prints the full description of the table first_table 
--- From the database hbtn_0c_0 in your MySQL server
-SHOW CREATE DATABASE first_table;
+-- Script that prints the full description of the table
+SHOW CREATE TABLE first_table;
