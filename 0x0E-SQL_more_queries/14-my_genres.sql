067e659ba29a860d55728059759c16a04c9b11e3
joan cruz <joancruz95@gmail.com>
2019-12-18 06:26:03 +0000

    Script that lists all genres in the show Dexter

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,11 @@
+-- Script that lists all genres in the show Dexter
+SELECT tv_genres.name
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON
+tv_show_genres.genre_id = tv_genres.id
+INNER JOIN tv_shows
+ON
+tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = 'DEXTER'
+ORDER BY tv_genres.name ASC
