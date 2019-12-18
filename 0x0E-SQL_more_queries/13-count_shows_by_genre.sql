b38ee561c223e0a235a00b1a2cf94b6603587f8a
joan cruz <joancruz95@gmail.com>
2019-12-18 06:20:28 +0000

    -- Script that lists all genres contained in hbtn_0d_tvshows

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- Script that lists all shows contained in hbtn_0d_tvshows
+SELECT tv_genres.name AS genre, count(*) AS number_of_shows
+FROM tv_show_genres
+LEFT JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
