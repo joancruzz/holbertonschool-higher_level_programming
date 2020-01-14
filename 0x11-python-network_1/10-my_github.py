a7611c43dde072b928fa3e0229a8827bb8d9efbb
joan cruz <joancruz95@gmail.com>
2020-01-14 08:04:47 +0000

    Edit white spaces

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
     import requests
     from sys import argv
     req = requests.get('https://api.github.com/user',
-                     auth=(argv[1], argv[2]))
+                       auth=(argv[1], argv[2]))
     print(req.json().get('id'))
