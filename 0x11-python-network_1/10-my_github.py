21a824a3f973d041bd1272eac9fb56e591b581b4
joan cruz <joancruz95@gmail.com>
2020-01-14 08:03:12 +0000

    Display Id with github api and credentials

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Python script that takes your Github credentials
+and uses the Github API to display your id
+"""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    req = requests.get('https://api.github.com/user',
+                     auth=(argv[1], argv[2]))
+    print(req.json().get('id'))
