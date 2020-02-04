0b5fd2a8575a6aedc001003e2a94864e0738831b
joan cruz <joancruz95@gmail.com>
2020-02-04 23:58:27 +0000

    Add conditional statement

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 // print process.argv
 const length = parseInt(process.argv.length);
-if (length >= 3) {
+if (length == 3) {
+  console.log('Argument found');
+} else if (length >= 4) {
   console.log('Arguments found');
 } else {
-  console.log('No argument');
+  console.log('No argument')
 }
