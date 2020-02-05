152a1654fc9e60772cf72861c4ec749b609b03e2
joan cruz <joancruz95@gmail.com>
2020-02-05 00:51:34 +0000

    add conditional statement

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -4,4 +4,8 @@ const first = process.argv[2];
 const third = process.argv[3];
 if (first && third) {
   console.log(first + ' is ' + third);
+} else if (first) {
+  console.log(first + ' is ' + 'undefined');
+} else {
+  console.log('undefined' + ' is ' + 'undefined');
 }
