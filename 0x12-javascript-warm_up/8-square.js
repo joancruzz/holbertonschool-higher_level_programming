872b927b73efe258b1a80a351c5d58aa435e106c
joan cruz <joancruz95@gmail.com>
2020-02-05 02:58:42 +0000

    Prints a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Prints a square
+const x = parseInt(process.argv[2]);
+if (x) {
+  for (let i = 0; i < x; i++) {
+    console.log('X'.repeat(x));
+  }
+} else {
+    console.log('Missing size');
+}
